e885456645c98fa016375b106a267ec503d5380d
misschiiff <misschiif>
2022-03-10 19:27:39 -0800

    4 puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
+
 /**
  *main - prints predefined text
  *Return: Always 0.
  */
 int main(void)
 {
-	puts("programming is like building a multilingual puzzle");
+	puts(" programming is like building a multilingual puzzle.");
 	return (0);
 }
