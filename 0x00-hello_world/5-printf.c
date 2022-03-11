d9da147f0b8b60542b6ad0239b0c1c4919f7e0d7
misschiiff <misschiif>
2022-03-10 19:52:17 -0800

    5 printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,19 @@
+/*
+ * file: 5-printf.c
+ *
+ * written by misschiiff
+ *
+ */
+#include <stdio.h>
+/**
+ * main - prints "with proper grammer, but the outcome is a piece of art,"
+ *
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0)
+}
